d8fd9446a3d95c4904a46322f0e3e63d2fad0383
naveganth <lucas.navegant@gmail.com>
2025-11-14 17:03:44 -0300

    new

---
+++ SimulStreamingProjeti/run-medium.bat
@@ -0,0 +1,1 @@
+python simulstreaming_whisper_server.py --language pt --task transcribe --host 0.0.0.0 --model_path ./models/medium.pt
---
+++ pkg/crate_whisper_wrapper_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_whisperwrapper_free: (a: number, b: number) => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
