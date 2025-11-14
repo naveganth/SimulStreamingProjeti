5b421e02844761a6737e6f7069693dafc4e3d28b
naveganth <lucas.navegant@gmail.com>
2025-11-14 17:57:19 -0300

    remove old pkg

--- pkg/crate_whisper_wrapper_bg.wasm.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const __wbg_whisperwrapper_free: (a: number, b: number) => void;
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_start: () => void;
